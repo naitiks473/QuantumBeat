ae4b1687002bb81bf65cf9a8be67cef5e51e9d71
naitiks473 <naitiks473@gmail.com>
2025-11-19 12:00:10 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@
  * 
  * @fileoverview 
  * @version 1.0.0
- * @author GlaceYT
+ * @author ARC
  */
 
 const DiscordRESTClientManager = require('discord.js').REST;
@@ -256,3 +256,4 @@ class SlashCommandRegistrationService {
         console.log('✅ Successfully registered slash commands!');
     }
 }
+
